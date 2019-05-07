43991deee5bce3316179b3377720ac42d465af3b
Altahoma <altahoma@gmail.com>
2019-05-08 02:47:28 +0300

    Revert "Revert "Completed part 8""
    This reverts commit ed01ebe77bdbe0570800a9f6006141825744fce7.

--- pets.py
+++ pets.py
@@ -1,7 +1,9 @@
-pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
-print(pets)
+def describe_pet(pet_name, animal_type='dog'):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
-while 'cat' in pets:
-    pets.remove('cat')
 
-print(pets)
+describe_pet('hamster', 'harry')
+describe_pet(animal_type='dog', pet_name='willie')
+describe_pet(pet_name='tomas', animal_type='cat')
+describe_pet(pet_name='Gav')
--- pizza.py
+++ pizza.py
@@ -1,10 +1,17 @@
-pizza = {
-    'crust': 'thick',
-    'toppings': ['mushrooms', 'extra cheese'],
-}
+# pizza = {
+#     'crust': 'thick',
+#     'toppings': ['mushrooms', 'extra cheese'],
+# }
+#
+# print("You ordered a " + pizza['crust'] + "-crust pizza " +
+#       "with the following toppings:")
+#
+# for topping in pizza['toppings']:
+#     print("\t" + topping)
 
-print("You ordered a " + pizza['crust'] + "-crust pizza " +
-      "with the following toppings:")
 
-for topping in pizza['toppings']:
-    print("\t" + topping)
+def make_pizza(size, *toppings):
+    print("\nMaking a " + str(size) +
+          "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
