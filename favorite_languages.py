e2d306a71854864c73939eccaf16e18757e03326
Altahoma <altahoma@gmail.com>
2019-05-08 16:15:27 +0300

    Completed part 9

--- favorite_languages.py
+++ favorite_languages.py
@@ -1,33 +1,12 @@
-# favorite_language = {
-#     'jen': 'python',
-#     'sarah': 'c',
-#     'edward': 'ruby',
-#     'phil': 'python',
-# }
-# for name, language in favorite_language.items():
-#     print(name.title() + "'s favorite language is " +
-#           language.title() + ".")
-#
-# for name in favorite_language.keys():
-#     print(name.title())
-#
-# for language in favorite_language.values():
-#     print(language.title())
-#
-# if 'erin' not in favorite_language.keys():
-#     print("\nErin, please take out poll!\n")
-#
-# for language in set(favorite_language.values()):
-#     print(language.title())
+from collections import OrderedDict
 
-favorite_language = {
-    'jen': ['python', 'ruby'],
-    'sarah': ['c'],
-    'edward': ['ruby', 'go'],
-    'phil': ['python', 'haskell'],
-}
+
+favorite_language = OrderedDict()
+
+favorite_language['jen'] = 'python'
+favorite_language['sarah'] = 'c'
+favorite_language['edward'] = 'ruby'
+favorite_language['phil'] = 'python'
 
 for name, languages in favorite_language.items():
-    print("\n" + name.title() + "'s favorite languages are:")
-    for language in languages:
-        print("\t" + language.title())
+    print(name.title() + "'s favorite language is " + languages.title() + ".")
